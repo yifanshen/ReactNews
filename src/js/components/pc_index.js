cdbbad9f0101dd11613c932a25e1e595aa142bbf
Yifan Shen <shenyif@oregonstate.edu>
2017-11-12 14:55:56 -0800

    update

---
+++ src/js/components/pc_index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import PCHeader from './pc_header';
+import PCFooter from './pc_footer';
+
+
+export default class PCIndex extends React.Component{
+	render(){
+		return(
+			<div>
+				<PCHeader> </PCHeader>
+				<PCFooter/>
+			</div>
+			);
+	};
+} 
